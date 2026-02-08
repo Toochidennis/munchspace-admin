022114514fdea6d738b3ed46318d55d9760a9fce
meshackdaniel <meshackdaniel225@gmail.com>
2026-02-08 19:23:47 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import Link from 'next/link';
 import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>Welcome to munchspace admin</div>
-  )
+    <div>
+      <h1>Welcome to munchspace admin</h1>
+      <p>Pages</p>
+      <div className="flex flex-col text-blue-500 underline">
+        <Link href={"/sign-in"}>Login</Link>
+        <Link href={"/reset-password"}>Reset Password</Link>
+        <Link href={"/change-password"}>Change Password</Link>
+        <Link href={"/admin/settings"}>Settings</Link>
+        <Link href={"/admin/orders"}>Orders</Link>
+        <Link href={"/admin/vendors"}>Vendors</Link>
+        <Link href={"/admin/customers"}>Customers</Link>
+        <Link href={"/admin/dashboard"}>Dashboard</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Homepage
